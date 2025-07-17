580a8ef7ed73541fe7a347568560354947b1198c
Mohammed Badawy <115544356+Mohammed-Badawy@users.noreply.github.com>
2025-07-17 15:18:01 +0300

    Answer pset7 movies queries

---
+++ pset7/movies/13.sql
@@ -0,0 +1,11 @@
+SELECT DISTINCT("name") FROM "people"
+JOIN "stars" ON people.id = stars.person_id
+JOIN "movies" ON stars.movie_id = movies.id
+WHERE movies.title IN (
+    SELECT DISTINCT("title") FROM "people"
+    JOIN "stars" ON people.id = stars.person_id
+    JOIN "movies" ON stars.movie_id = movies.id
+    WHERE "name" = "Kevin Bacon"
+    AND "birth" = 1958
+)   
+AND "name" != "Kevin Bacon";
